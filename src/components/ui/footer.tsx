a3af1edfdd9be2b6ab679d5c9b7bf80e2ee7744f
jakun9999 <matthieulucas457@outlook.fr>
2026-09-17 11:10:27 +0200

    Footer - alternative text completed to avoid wave alert.

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="flex items-center justify-between w-full h-[69.36px] px-10 bg-kasa-white border-t border-kasa-gray-light">
       <Image
         src="/logos/kasa_logo_picto.svg"
-        alt="Logo Kasa"
+        alt="Logo Kasa pied de page"
         width={46}
         height={53}
       />
