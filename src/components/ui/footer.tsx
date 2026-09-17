396a1a70c5be3a9ebdcfcf4af0bbb440f41dc57f
jakun9999 <matthieulucas457@outlook.fr>
2026-09-17 11:07:48 +0200

    Footer - removed redundant link (wave alert).

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export function Footer() {
   return (
     <footer className="flex items-center justify-between w-full h-[69.36px] px-10 bg-kasa-white border-t border-kasa-gray-light">
-      <Link href="/login">
-        <Image
-          src="/logos/kasa_logo_picto.svg"
-          alt="Logo Kasa"
-          width={46}
-          height={53}
-        />
-      </Link>
+      <Image
+        src="/logos/kasa_logo_picto.svg"
+        alt="Logo Kasa"
+        width={46}
+        height={53}
+      />
       <p className="text-kasa-gray-dark text-[12px] font-medium">
         © 2020 Kasa. All rights reserved.
       </p>
